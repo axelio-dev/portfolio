b73ae3f680cd74c60faa954db5f65de8d136acf7
axelio-dev <axel.barth@yahoo.com>
2025-10-14 22:01:41 +0200

    Modified the stats component. It was in flex mode and caused conflicts in the responsive mode. Switech to grid mode

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -17,18 +17,18 @@ export default function Stats() {
   const totalHours = (data.total_seconds / 3600).toFixed(0);
 
   return (
-    <div className="flex justify-around gap-15 mr-[40px] ml-[40px]">
-      <div className="flex-1 flex flex-col items-center justify-center text-center p-6 bg-white/10 backdrop-blur-md border border-white/20 rounded-2xl shadow-md transition-transform duration-500 ease-in-out hover:scale-105 hover:shadow-blue-500/40 hover:border-blue-500">
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-15 mr-[40px] ml-[40px]">
+      <div className="flex flex-col items-center justify-center text-center p-6 bg-white/10 backdrop-blur-md border border-white/20 rounded-2xl shadow-md transition-transform duration-500 ease-in-out hover:scale-105 hover:shadow-blue-500/40 hover:border-blue-500">
         <div className="text-4xl text-white font-bold">{totalHours}</div>
         <div className="mt-2 text-gray-300">Coding Hours</div>
       </div>
 
-      <div className="flex-1 flex flex-col items-center justify-center text-center p-6 bg-white/10 backdrop-blur-md border border-white/20 rounded-2xl shadow-md transition-transform duration-500 ease-in-out hover:scale-105 hover:shadow-blue-500/40 hover:border-blue-500">
+      <div className="flex flex-col items-center justify-center text-center p-6 bg-white/10 backdrop-blur-md border border-white/20 rounded-2xl shadow-md transition-transform duration-500 ease-in-out hover:scale-105 hover:shadow-blue-500/40 hover:border-blue-500">
         <div className="text-4xl text-white font-bold">1</div>
         <div className="mt-2 text-gray-300">Projects completed</div>
       </div>
 
-      <div className="flex-1 flex flex-col items-center justify-center text-center p-6 bg-white/10 backdrop-blur-md border border-white/20 rounded-2xl shadow-md transition-transform duration-500 ease-in-out hover:scale-105 hover:shadow-blue-500/40 hover:border-blue-500">
+      <div className="flex flex-col items-center justify-center text-center p-6 bg-white/10 backdrop-blur-md border border-white/20 rounded-2xl shadow-md transition-transform duration-500 ease-in-out hover:scale-105 hover:shadow-blue-500/40 hover:border-blue-500">
         <div className="text-4xl text-white font-bold">0</div>
         <div className="mt-2 text-gray-300">Stars collected</div>
       </div>
