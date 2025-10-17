26739096e7a59b86c72addcccd02af514d006054
axelio-dev <axel.barth@yahoo.com>
2025-10-17 12:12:28 +0200

    Fixed the width for the responsive contact section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -12,7 +12,7 @@ export default function Contact({
   icon?: React.ReactNode;
 }) {
   return (
-    <div className="w-[400px] h-[150px] md:w-[500px] md:h-[175px] border border-white/20 rounded-2xl bg-white/10 backdrop-blur-md p-6 flex flex-col items-center justify-center text-center shadow-md transition-transform duration-500 ease-in-out hover:scale-105 hover:shadow-blue-500/40 hover:border-blue-500">
+    <div className="w-[350px] h-[160px] md:w-[500px] md:h-[175px] border border-white/20 rounded-2xl bg-white/10 backdrop-blur-md p-6 flex flex-col items-center justify-center text-center shadow-md transition-transform duration-500 ease-in-out hover:scale-105 hover:shadow-blue-500/40 hover:border-blue-500">
       <div className="mx-auto flex flex-col gap-3 items-center">
         {icon && <div className="text-blue-400 mb-1">{icon}</div>}
         <h3 className="text-white text-xl text-center mt-[10px]">{title}</h3>
